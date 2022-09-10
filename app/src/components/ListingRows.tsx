027ae4da43a251657475c84da1371789a868e760
Armands <armands.dunskis@gmail.com>
2022-09-10 16:48:27 +0100

    basic budget table

--- app/src/components/ListingRows.tsx
+++ app/src/components/ListingRows.tsx
@@ -1,7 +1,24 @@
+// React
+import exp from 'constants';
+import React, { createElement } from 'react';
 
-function ListingRows(){
-  const e = <tr></tr>
-  return e
+function ListingRows(props: any){
+
+  var incomes_td = props.incomes.map( (income : number) =>
+    <td>{income}</td>
+  )
+
+  var expenses_td = props.expenses.map( (expense : number) => 
+    <td>{expense}</td>
+  )
+  
+  var listing_rows = []
+  for(var i = 0; i < props.max_rows; i++){
+    var row = <tr key={i}>{expenses_td.at(i)}{incomes_td.at(i)}</tr>
+    listing_rows.push(row)
+  }
+
+  return <tbody id='listings'>{listing_rows}</tbody>
 }
 
 export {ListingRows}
--- app/src/components/Table.tsx
+++ app/src/components/Table.tsx
@@ -1,22 +1,35 @@
+// React
+import React from 'react';
 // Components
 import {ListingRows} from './ListingRows'
 
 function Table(){
   const headings =
-    <tr>
-      <th>Expenses</th>
-      <th>Income</th>
-    </tr>
+    <thead>
+      <tr>
+        <th>Expenses</th>
+        <th>Income</th>
+      </tr>
+    </thead>
 
   const data = require("../sample.json")
   const expenses = data.expenses
   const incomes = data.incomes
   let max_rows :number = (expenses.length > incomes.length ? expenses.length : incomes.length )
-  const listingRows = <ListingRows></ListingRows>
+  
+  const p = {
+    expenses: expenses,
+    incomes: incomes,
+    max_rows: max_rows,
+  }
+
+  // spread, unpacks all properties as a prop
+  const listingRows = <ListingRows {...p}></ListingRows>
   
   const table =
     <table>
-      {headings}
+        {headings}
+        {listingRows}
     </table>
   
   return table
