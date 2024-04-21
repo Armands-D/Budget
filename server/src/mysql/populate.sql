6591a10e899900307bc8ecc32a8bfc418098edea
Armands Dunskis <armands.dunskis@gmail.com>
2024-04-21 20:52:18 +0100

    Added server code, restructured dirs, added MySQL query

---
+++ server/src/mysql/populate.sql
@@ -0,0 +1,28 @@
+
+INSERT INTO main_db.user (username, email, password)
+VALUES (armands, a.d@g.com, armands);
+
+SET @userId = LAST_INSERT_ID();
+
+INSERT INTO main_db.budget (userId)
+VALUES (@userId);
+
+SET @budgetId = LAST_INSERT_ID();
+SET @income = 'INCOME';
+SET @expense = 'EXPENSE';
+
+INSERT INTO main_db.active_categories (budgetId, name, type)
+VALUES(@budgetId, 'House Hold', @expense);
+
+SET @categoryId = LAST_INSERT_ID();
+
+INSERT INTO main_db.active_entries (categoryId, name, amount)
+VALUES(@categoryId, 'Rent', 1200);
+INSERT INTO main_db.active_entries (categoryId, name, amount)
+VALUES(@categoryId, 'Groceries', 200);
+
+# Select all users and their budgets
+SELECT u.username, u.id as userId, b.id as budgetId, c.type as type, c.name as category, e.name as entry, e.amount as amount  FROM main_db.user as u
+    LEFT JOIN main_db.budget AS b ON b.userId = u.id
+    LEFT JOIN main_db.active_categories as c ON c.budgetId = b.id
+    LEFT JOIN main_db.active_entries as e ON e.categoryId = c.id
