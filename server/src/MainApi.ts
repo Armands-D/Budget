f9277be00b4f26b49bb159748ba02cab273b97d3
Armands Dunskis <armands.dunskis@gmail.com>
2024-04-21 23:12:50 +0100

    Added API endpoint with SQL conenction and ENV file

--- server/src/MainApi.ts
+++ server/src/MainApi.ts
@@ -1,7 +1,14 @@
 import express, { Request, Response } from 'express';
+import * as mysql from 'mysql2';
+import 'dotenv/config'
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const DB_PASSWORD = process.env.DB_PASSWORD || 'NULL'
+const DB_USERNAME = process.env.DB_USERNAME || 'NULL'
+const DB_HOST = process.env.DB_HOST || 'NULL'
+const DB_PORT = Number(process.env.DB_PORT)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
@@ -10,3 +17,31 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+app.get('/user', (req: Request, res: Response) => {
+  let connection = mysql.createConnection({
+    host      : DB_HOST,
+    port      : DB_PORT,
+    user      : DB_USERNAME,
+    password  : DB_PASSWORD
+  });
+  
+  connection.connect(function(err: mysql.QueryError | null):void{
+    if (err) {
+      console.error('error connecting: ' + err.stack)
+      let connection_error : ApiError = {error: 'Client error', status: 400, message: 'Failed to Connect to DB'}
+      res.send(connection_error)
+      return
+    }
+    console.log('connected as id ' + connection.threadId);
+    res.send({'ping': 'pong'})
+  });
+
+})
+
+
+interface ApiError{
+  error: string
+  status: number
+  message: string
+}
