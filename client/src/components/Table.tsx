cffb84157215e648618b418c0d65ed7268811606
Armands Dunskis <armands.dunskis@gmail.com>
2024-04-26 20:39:07 +0100

    Updated build table

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -7,7 +7,7 @@ function Table (){
   let userId : number = 1
   let budgetId : number = 1
 
-  const [budget, setBudget] = React.useState<any>({})
+  const [budget, setBudget] = React.useState<any>(null)
   const [budgetUpdate, setBudgetUpdate] = React.useState<any>(true)
 
   React.useEffect(( ) => {
@@ -45,21 +45,49 @@ function Table (){
     setBudgetUpdate(!budgetUpdate)
   }
 
+  function buildTable(budget: any){
+
+
+    if(! budget) return
+
+    let num_categories = Object.keys(budget.expenses).length
+    let num_entries = 0
+    for (var cat in budget.expenses){
+      num_entries += budget.expenses[cat].entries.length
+    }
+    let rowSpan = num_categories + num_entries + 1 // For Self
+
+    let rows = []
+
+    for (var cat in budget.expenses){
+      rows.push(<tr><th>{cat}</th></tr>)
+      for(var entry of budget.expenses[cat].entries){
+        rows.push(<tr><th>{JSON.stringify(entry)}</th></tr>)
+      }
+    }
+    return <tbody>
+      <tr><th rowSpan={rowSpan}>EXPENSES</th></tr>
+      {rows}
+    </tbody>
+  }
+
   const headings =
-    <thead>
-      <tr>
-      </tr>
-        {buildCategories('expenses', {...budget})}
-      <tr>
-      </tr>
-        {buildCategories('income', {...budget})}
-    </thead>
+  <tbody>
+  </tbody>
+    // <thead>
+    //   <tr>
+    //   </tr>
+    //     {buildCategories('expenses', {...budget})}
+    //   <tr>
+    //   </tr>
+    //     {buildCategories('income', {...budget})}
+    // </thead>
 
   
   const table =
     <div>
       <table>
-          {headings}
+          {buildTable(budget)}
       </table>
       <button onClick={f}>{String(budgetUpdate)}</button>
     </div>
