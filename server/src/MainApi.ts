fc478246f6b9ad6f3a6e0c5ba27e455be52e3c9c
Armands Dunskis <armands.dunskis@gmail.com>
2024-07-26 22:30:46 +0100

    Wrapped DB connection into safety function

--- server/src/MainApi.ts
+++ server/src/MainApi.ts
@@ -24,6 +24,17 @@ const DatabaseConnection = () :Promise<mysql.Connection> => {
   });
 }
 
+async function safeDBConnection(fun: (conenction: mysql.Connection) => Promise<void>){
+  try{
+    return DatabaseConnection()
+    .then((connection: mysql.Connection)=>{
+      fun(connection).then(() => connection.end())
+    })
+  }catch(error: Error | unknown){
+    console.log(error)
+  }
+}
+
 app.use(cors({origin: "http://localhost:3000",credentials: true,}))
 app.use(express.json())
 app.use(cookieParser());
@@ -46,58 +57,37 @@ app.get('/user/:userId/budget/:budgetId', (req: Request, res: Response) => {
     res.send(param_error)
     return
   }
-  
-  let connection: mysql.Connection = DatabaseConnection()
-  connection.connect(function(err: mysql.QueryError | null):void{
 
-    if (err) {
-      console.error('error connecting: ' + err.stack)
-      let connection_error : ApiError = {
-        error: 'Client error',
-        status: 400,
-        message: 'Failed to Connect to DB'
+  safeDBConnection(async(connection: mysql.Connection)=>{
+    let [results, fields]: [mysql.RowDataPacket[], mysql.FieldPacket[]]  =
+      await connection.query(
+        `CALL main_db.get_budget(?, ?);`,
+        [userId, budgetId],
+    )
+    
+    console.log('result:', results)
+    console.log(JSON.parse(JSON.stringify(results))[1])
+
+    let result_json : UserBudget.Results = JSON.parse(JSON.stringify(results))[0]
+
+    let budget : UserBudget.Reponse = {id: budgetId, income: {}, expenses: {}}
+    for (var row_index in result_json) {
+      var row = result_json[row_index]
+      let type : Record<string, UserBudget.Category> = row.type === 'INCOME' ? budget.income : budget.expenses
+      let category_id: string = String(row.categoryId)
+      if (! type.hasOwnProperty(category_id)) {
+        type[category_id] = {
+          name: row.category,
+          entries: []
+        }
       }
-      res.send(connection_error)
-      return
+      let entry: UserBudget.Entry = {id: row.entryId, name: row.name, amount: row.amount}
+      type[category_id].entries.push(entry)
     }
 
-    connection.query(
-      `CALL main_db.get_budget(?, ?);`,
-      [userId, budgetId],
-      function (
-        err: mysql.QueryError | null,
-        result: mysql.QueryResult,
-        fileds: mysql.FieldPacket[]
-      ){
-
-        if (err) throw err;
-        console.log('result:', result)
-        console.log(JSON.parse(JSON.stringify(result))[1])
-
-        let result_json : UserBudget.Results = JSON.parse(JSON.stringify(result))[0]
-
-        let budget : UserBudget.Reponse = {id: budgetId, income: {}, expenses: {}}
-        for (var row_index in result_json) {
-          var row = result_json[row_index]
-          let type : Record<string, UserBudget.Category> = row.type === 'INCOME' ? budget.income : budget.expenses
-          let category_id: string = String(row.categoryId)
-          if (! type.hasOwnProperty(category_id)) {
-            type[category_id] = {
-              name: row.category,
-              entries: []
-            }
-          }
-          let entry: UserBudget.Entry = {id: row.entryId, name: row.name, amount: row.amount}
-          type[category_id].entries.push(entry)
-        }
-
-        console.log('transformed res: ', JSON.stringify(budget, null, 2))
-        res.status(200).send(budget)
-      }
-    )
-
-    connection.end()
-  });
+    console.log('transformed res: ', JSON.stringify(budget, null, 2))
+    res.status(200).send(budget)
+  })
 })
 
 app.post('/login', async (req, res) => {
@@ -118,92 +108,82 @@ app.post('/login', async (req, res) => {
   let password : string = String(req.body.password)
   // TODO: Check if valid email
 
+  safeDBConnection(async (connection: mysql.Connection)=>{
+    let [results, fields] : [mysql.RowDataPacket[], mysql.FieldPacket[]] =
+      await connection.query<mysql.RowDataPacket[]>(
+      `SELECT
+      	u.username AS userName,
+		    u.email,
+        u.password,
+		    u.id AS userId
+      FROM main_db.user AS u
+        WHERE email = ?`,
+      [email]
+    )
 
-  // connection.connect(function(err: mysql.QueryError | null ): void{
-    // if (err) return sendApiError(res, Database.error_db_connect)
+    let loginError: ApiError | null = validateLogin(results, fields)
 
-  try{
-    DatabaseConnection()
-    .then(async (connection : mysql.Connection) => {
-      let [results, fields] : [mysql.RowDataPacket[], mysql.FieldPacket[]] =
-        await connection.query<mysql.RowDataPacket[]>(
-        `SELECT
-        	u.username AS userName,
-		      u.email,
-          u.password,
-		      u.id AS userId
-        FROM main_db.user AS u
-          WHERE email = ?`,
-        [email]
-      )
+    if(loginError) return sendApiError(res, loginError)
+    
+    let user_info : Login.UserDetails = JSON.parse(JSON.stringify(results))[0]
 
-      let loginError: ApiError | null = validateLogin(results, fields)
+    getAuthToken(connection, user_info)
+  }).then(()=> console.log("LAST THING"))
+  // TODO: Make this work
 
-      if(loginError) return sendApiError(res, loginError)
-      
-      let user_info : Login.UserDetails = JSON.parse(JSON.stringify(results))[0]
+  function validateLogin(
+      result: mysql.RowDataPacket[],
+      fields: mysql.FieldPacket[]
+  ): ApiError | null {
 
-      getAuthToken(connection, user_info)
-    })
-  }catch(error: Error | unknown){
-    console.log(error)
+    console.log('Validate Login result:', result)
+    if(!result.length) return Login.error_auth
+    if(result.length > 1) return Login.error_multiple_users
+
+    let user: Login.UserDetails = JSON.parse(JSON.stringify(result))[0]
+    console.log('result_json', user)
+
+    if(!(password === user.password)) return Login.error_auth
+    return null
+
+    // return getAuthToken(user)
+
+    // try{
+    //   argon2.verify(
+    //     // password
+    //     '$argon2id$v=19$m=65536,t=3,p=4$cGFzc3dvcmQ$LL7xx04g0QX5dNIvbRdNXWMWchh8E8ZM4ZwMr/iSJqs',
+    //     password,
+    //   ).then((verified: boolean)=>{
+    //     if(!verified) return sendApiError(res, Login.error_auth)
+    //     res.send({token: "token"})
+    //   })
+    // }catch(e){
+    //   console.error('--- Argon2Id Error')
+    //   throw e
+    // }
   }
 
-    function validateLogin(
-        result: mysql.RowDataPacket[],
-        fields: mysql.FieldPacket[]
-    ): ApiError | null {
-
-      console.log('Validate Login result:', result)
-      if(!result.length) return Login.error_auth
-      if(result.length > 1) return Login.error_multiple_users
-
-      let user: Login.UserDetails = JSON.parse(JSON.stringify(result))[0]
-      console.log('result_json', user)
-
-      if(!(password === user.password)) return Login.error_auth
-      return null
-
-      // return getAuthToken(user)
-
-      // try{
-      //   argon2.verify(
-      //     // password
-      //     '$argon2id$v=19$m=65536,t=3,p=4$cGFzc3dvcmQ$LL7xx04g0QX5dNIvbRdNXWMWchh8E8ZM4ZwMr/iSJqs',
-      //     password,
-      //   ).then((verified: boolean)=>{
-      //     if(!verified) return sendApiError(res, Login.error_auth)
-      //     res.send({token: "token"})
-      //   })
-      // }catch(e){
-      //   console.error('--- Argon2Id Error')
-      //   throw e
-      // }
-    }
-
-    async function getAuthToken(connection: mysql.Connection, user: Login.UserDetails) {
-      connection.query<mysql.ProcedureCallPacket<mysql.ResultSetHeader>>(
-        "CALL main_db.refresh_auth_token(?)",
-        [user.userId]
-      ).then((
-        [result, fields] : [mysql.ResultSetHeader, mysql.FieldPacket[]]
-      )=>{
-          let rows: mysql.RowDataPacket[] = result[0]
-          let header: mysql.ResultSetHeader = result[1]
-          console.log("GetAuthToken rows:", rows, "header: ", header)
-          let user_details: Login.UserDetails = JSON.parse(JSON.stringify(rows[0]))
-          res.cookie("token", user_details.token, {
-            httpOnly: true,
-            path: "/",
-            domain: "localhost",
-            secure: false,
-            sameSite: "none",
-            maxAge: 1000 * 60 * 60,
-          })
-          res.send({token: user_details.token})
-      })
-    }
-  // })
+  async function getAuthToken(connection: mysql.Connection, user: Login.UserDetails) {
+    connection.query<mysql.ProcedureCallPacket<mysql.RowDataPacket[]>>(
+      "CALL main_db.refresh_auth_token(?)",
+      [user.userId]
+    ).then((
+      [[rows, header], fields]
+      : [[mysql.RowDataPacket[], mysql.ResultSetHeader], mysql.FieldPacket[]]
+    )=>{
+        console.log("GetAuthToken rows:", rows, "header: ", header)
+        let user_details: Login.UserDetails = JSON.parse(JSON.stringify(rows[0]))
+        res.cookie("token", user_details.token, {
+          httpOnly: true,
+          path: "/",
+          domain: "localhost",
+          secure: false,
+          sameSite: "none",
+          maxAge: 1000 * 60 * 60,
+        })
+        res.send({token: user_details.token})
+    })
+  }
 })
 
 app.post('/create-token', (req, res)=>{
